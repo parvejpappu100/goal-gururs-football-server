d981f9c8b2cc0098751d8779d83daa2b54fcec33
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-06-12 01:06:54 +0600

    insert selected class on database

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdnsrak.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdnsrak.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,6 +24,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const classesCollection = client.db("goalGurusDb").collection("classes");
+    const coachesCollection = client.db("goalGurusDb").collection("coaches");
+    const cartsCollection = client.db("goalGurusDb").collection("carts");
+
+    // * Classes api:
+    app.get("/classes" , async(req , res) => {
+        const result = await classesCollection.find().toArray();
+        res.send(result);
+    });
+
+    // * Coaches Api:
+    app.get("/coaches" , async(req , res) => {
+        const result = await coachesCollection.find().toArray();
+        res.send(result);
+    });
+
+    // * Carts api:
+    app.post("/carts" , async(req , res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartsCollection.insertOne(item);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
